917f2796b27380b73a95f2a9c483b0fc297fdc68
Eduardo Jesus <eduuhjesus14@gmail.com>
2025-06-23 23:25:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import alg_hillclimbing as hillclimbing
 TAM_CELULA = 60
 NUM_RAINHAS = 8
 LARGURA = TAM_CELULA * NUM_RAINHAS
-ALTURA = TAM_CELULA * NUM_RAINHAS + 50  # espaço extra para o botão
+ALTURA = TAM_CELULA * NUM_RAINHAS + 100
 BRANCO = (255, 255, 255)
 PRETO = (0, 0, 0)
 VERMELHO = (255, 0, 0)
@@ -65,33 +65,46 @@ def main():
     clock = pygame.time.Clock()
     clock.tick(60)
     screen.fill(BRANCO)
-    botao_genetico = botao(200, 40, 10, ALTURA - 50, AZUL, BRANCO, "GENETICO", genetico.algoritmo_genetico)
-    botao_hillclimbing = botao(200, 40, 250, ALTURA - 50, VERMELHO, BRANCO, "HILLCLIMBING", hillclimbing.solve)
+
+    botao_genetico = botao(200, 40, 10, ALTURA - 50, AZUL, BRANCO, "GENETICO (1)", genetico.algoritmo_genetico)
+    botao_hillclimbing = botao(200, 40, 250, ALTURA - 50, VERMELHO, BRANCO, "HILLCLIMBING (1)", hillclimbing.solve)
+    botao_genetico_92 = botao(200, 40, 10, ALTURA - 100, CINZA, PRETO, "GENÉTICO (92)", genetico.encontrar_92_solucoes_genetico)
+    botao_hillclimbing_92 = botao(200, 40, 250, ALTURA - 100, (100,100,100), (255,255,255), "HILLCLIMBING (92)", hillclimbing.encontrar_92_solucoes_hillclimbing)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 resposta1 = botao_genetico.handle_click(event)
                 resposta2 = botao_hillclimbing.handle_click(event)
+                resposta3 = botao_genetico_92.handle_click(event)
+                resposta4 = botao_hillclimbing_92.handle_click(event)
+
+
+
                 if resposta1:
                     solucao_atual = resposta1
                 elif resposta2:
                     solucao_atual = [next(i for i in range(len(resposta2)) if resposta2[i][col] == 1) for col in range(NUM_RAINHAS)]
-                    # precisa dessa list comprehension pra poder inverter o resultado que saiu do algoritmo,
-                    # acabei fazendo o inverso do que "desenhar_rainhas()" precisa pra funcionar, como o outro algoritmo
-                    # retorna as rainhas no formato ideal, decidi por não reescrever nada e só resolver o problema inline mesmo.
-
+                elif resposta3:
+                    if isinstance(resposta3, list) and len(resposta3) > 0:
+                        solucao_atual = list(resposta3[0])  # Mostra a primeira das 92 soluções
+                elif resposta4:
+                    if isinstance(resposta4, list) and len(resposta4) > 0:
+                        solucao_atual = list(resposta4[0])  # Mostra a primeira solução encontrada pelo hill climbing
 
         desenhar_tabuleiro()
         desenhar_rainhas(solucao_atual)
         botao_genetico.draw()
         botao_hillclimbing.draw()
-        pygame.display.update()
+        botao_genetico_92.draw()
+        botao_hillclimbing_92.draw()
 
-    sys.exit()
+        pygame.display.update()
 
 if __name__ == "__main__":
     main()
